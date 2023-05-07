9b2e87b004fb48d0e7aaa956d490c5cf10abac70
derevoAM <derevianchenko.ma@phystech.edu>
2023-05-07 17:36:29 +0300

    cmake-build-debug removed

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derevo/Projects/Orbital_maneuvers/tests
-# Build directory: /home/derevo/Projects/Orbital_maneuvers/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
